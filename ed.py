29d515bc8d4f5c7a39817e1ba8e144665f3bb3f3
Vamsi <vamsir@gmail.com>
2020-11-12 03:29:26 -0500

    adding chrome options

--- ed.py
+++ ed.py
@@ -3,14 +3,26 @@ from bs4 import  BeautifulSoup
 from time import sleep
 from selenium import webdriver
 import pandas as pd
-
+from selenium.webdriver.chrome.options import Options
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
 
 fname = 'et.csv'
 df = pd.read_csv(fname,header = None)
 tl = list(df[0])[:3]
 dfEar = pd.DataFrame(columns = ['Ticker','Date'])
 
-driver = webdriver.Chrome(service_log_path='/earn-date')
+driver = webdriver.Chrome(options=chrome_options)
 
 for i in tl:
     url = 'https://ycharts.com/companies/'+i+'/events/#/?eventTypes=earnings,&pageNum=1'
@@ -29,3 +41,4 @@ for i in tl:
     sleep(10)
 
 dfEar.to_csv('earDateYC.csv', index = False)
+driver.close()
