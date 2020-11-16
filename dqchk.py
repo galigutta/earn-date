150d2e1fc10f71eeea045ade86a3c295ac074b6c
Vamsi <vamsir@gmail.com>
2020-11-16 17:23:03 -0500

    some progress on dqcheck

--- dqchk.py
+++ dqchk.py
@@ -7,36 +7,50 @@ This is a temporary script file.
 
 # importing pandas as pd 
 import pandas as pd 
+import numpy as np
+import datetime as dt
 
 df = pd.read_csv('SECFileTimes.csv')
 df.columns=['Ticker']+ list(df.columns[1:])
-
-#df = df[df['Ticker']!='TSLA' ]
+#filtering problem tickers
+df = df[(df['Ticker']!='TSLA') & (df['Ticker']!='DHR')]
+#df = df[df['Ticker']!='DHR']
 
 dfyc = pd.read_csv('edate.csv',parse_dates = [1])
 dfyc['yc']='yc'
+yc_procdate = dt.datetime.strptime('2020/11/12','%Y/%m/%d')
 
-dfyc = dfyc[dfyc['Ticker']!='GOOGL' ]
-dfyc = dfyc[dfyc['Ticker']!='BABA' ]
-#dfyc = dfyc[dfyc['Ticker']!='TSLA' ]
+#filtering problem tickers
+dfyc = dfyc[(dfyc['Ticker']!='GOOGL') & (dfyc['Ticker']!='BABA')&(dfyc['Ticker']!='TSLA')&(dfyc['Ticker']!='DHR')]
 
-dfyc = dfyc[dfyc['Date'].astype('str').str.slice(0, 4)!='2021']
+#dfyc = dfyc[dfyc['Date'].astype('str').str.slice(0, 4)!='2021']
 
+#Retain earnings from 2015 and later only
 dfyc = dfyc[dfyc['Date'].astype('str').str.slice(0, 4).astype('int')>2014]
 
-
-df['Date'] = pd.to_datetime( df['earn_datetime'].str.slice(0, 10)).dt.date
-df['Date'] = df['Date'].astype('datetime64[ns]')
-dfyc['Date'] = pd.to_datetime(dfyc['Date'],utc = True).astype('datetime64[ns]')
+# convert both to a datetime format and drop time to join
+df['Date'] = pd.to_datetime( df['earn_datetime'].str.slice(0, 10),utc = True).astype('datetime64[ns]')
+dfyc['Date'] = pd.to_datetime(dfyc['Date'].astype('str').str.slice(0, 10),utc = True).astype('datetime64[ns]')
 
 dfm = df.merge(dfyc, on =['Ticker','Date'], how = 'outer')
+dfm['effEarnDate'] = np.NaN
+#Retain all future dates from yc
 
 
-print(df.dtypes)
+#print(df.dtypes)
 print(dfyc.dtypes)
 
 dfmnan = dfm[dfm['yc'].isnull() | dfm['AfterClose'].isnull()  ]
 
-dfq = dfmnan.groupby(['Ticker'])['Ticker'].count().sort_values()
+dfq = dfmnan.groupby(['Ticker'])['Ticker'].count().sort_values(ascending = False)
 
-dfmnanticker = dfmnan[dfmnan['Ticker']=='DHR']
+
+
+ticker = 'WFC'
+dfmnanticker = dfmnan[dfmnan['Ticker']==ticker]
+dfmallticker = dfm[dfm['Ticker']==ticker].sort_values('Date', ascending = False)
+
+
+print (dfmallticker.shape)
+print(dfmallticker)
+print(dfq.head())
